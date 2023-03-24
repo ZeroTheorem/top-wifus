5266b2a3bb44ad4efa949691f8dbee11632667d7
ZeroTheorem <zeratuliche@gmail.com>
2023-03-24 17:07:53 +0300

    my com

--- app.py
+++ app.py
@@ -2,22 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+score = {
+    "Rias": 9.8,
+    "Asia": 8.0,
+    "Akeno": 9.9,
+    "Koneko": 7.0
+}
+
 
 @app.route("/")
 def test():
-    score = {
-        "Rias": 9.8,
-        "Asia": 8.0,
-        "Akeno": 9.9,
-        "Koneko": 7.0
-    }
-
     return render_template("index.html", score=score)
 
 
 @app.route("/Rias")
 def vote_Rias():
     return render_template("vote_Rias.html")
+
+
+@app.route("/Asia")
+def vote_Asia():
+    return render_template("vote_Asia.html")
+
+
+@app.route("/Akeno")
+def vote_Akeno():
+    return render_template("vote_Akeno.html")
+
+
+@app.route("/Koneko")
+def vote_Koneko():
+    return render_template("vote_Koneko.html")
 
 
 if __name__ == '__main__':
