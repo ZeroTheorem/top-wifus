c6cb230234071b9eba8d3200adfb3257e9d7c02a
ZeroTheorem <zeratuliche@gmail.com>
2023-03-24 20:16:09 +0300

    my com

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 score = {
-    "Rias": 9.8,
+    "Rias": 9.0,
     "Asia": 8.0,
     "Akeno": 9.9,
     "Koneko": 7.0
@@ -15,23 +15,43 @@ def test():
     return render_template("index.html", score=score)
 
 
-@app.route("/Rias")
+@app.route("/Rias", methods=['POST', 'GET'])
 def vote_Rias():
+    if request.method == 'POST':
+        score_Rias = request.form['vote_Rias']
+        score["Rias"] = score_Rias
+        return redirect("/")
+
     return render_template("vote_Rias.html")
 
 
-@app.route("/Asia")
+@app.route("/Asia", methods=['POST', 'GET'])
 def vote_Asia():
+    if request.method == 'POST':
+        score_Asia = request.form['vote_Asia']
+        score["Asia"] = score_Asia
+        return redirect("/")
+
     return render_template("vote_Asia.html")
 
 
-@app.route("/Akeno")
+@app.route("/Akeno", methods=['POST', 'GET'])
 def vote_Akeno():
+    if request.method == 'POST':
+        score_Akeno = request.form['vote_Akeno']
+        score["Akeno"] = score_Akeno
+        return redirect("/")
+
     return render_template("vote_Akeno.html")
 
 
-@app.route("/Koneko")
+@app.route("/Koneko", methods=['POST', 'GET'])
 def vote_Koneko():
+    if request.method == 'POST':
+        score_Koneko = request.form['vote_Koneko']
+        score["Koneko"] = score_Koneko
+        return redirect("/")
+
     return render_template("vote_Koneko.html")
 
 
