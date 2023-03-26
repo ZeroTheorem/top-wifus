a905124f9841c0182314ce85312dbe395baaa3a5
ZeroTheorem <zeratuliche@gmail.com>
2023-03-26 13:43:50 +0300

    my com

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ def test():
             curs.execute(f"UPDATE topwifus SET ASIA={result}")
             conn.commit()
             return redirect("/")
+        if 'vote_Akeno' in request.form:
+            result = request.form['vote_Akeno']
+            curs.execute(f"UPDATE topwifus SET AKENO={result}")
+            conn.commit()
+            return redirect("/")
+        if 'vote_Koneko' in request.form:
+            result = request.form['vote_Koneko']
+            curs.execute(f"UPDATE topwifus SET KONEKO={result}")
+            conn.commit()
+            return redirect("/")
 
     curs.execute("SELECT * FROM topwifus")
     reuslt = curs.fetchone()
@@ -31,50 +41,6 @@ def test():
              "Koneko": reuslt[3]}
 
     return render_template("index.html", score=score)
-
-
-@app.route("/Rias", methods=['POST', 'GET'])
-def vote_Rias():
-    if request.method == 'POST':
-        result = request.form['vote_Rias']
-        curs.execute(f"UPDATE topwifus SET RIAS={result}")
-        conn.commit()
-        return redirect("/")
-
-    return render_template("vote_Rias.html")
-
-
-@app.route("/Asia", methods=['POST', 'GET'])
-def vote_Asia():
-    if request.method == 'POST':
-        result = request.form['vote_Asia']
-        curs.execute(f"UPDATE topwifus SET ASIA={result}")
-        conn.commit()
-        return redirect("/")
-
-    return render_template("vote_Asia.html")
-
-
-@app.route("/Akeno", methods=['POST', 'GET'])
-def vote_Akeno():
-    if request.method == 'POST':
-        result = request.form['vote_Akeno']
-        curs.execute(f"UPDATE topwifus SET AKENO={result}")
-        conn.commit()
-        return redirect("/")
-
-    return render_template("vote_Akeno.html")
-
-
-@app.route("/Koneko", methods=['POST', 'GET'])
-def vote_Koneko():
-    if request.method == 'POST':
-        result = request.form['vote_Koneko']
-        curs.execute(f"UPDATE topwifus SET KONEKO={result}")
-        conn.commit()
-        return redirect("/")
-
-    return render_template("vote_Koneko.html")
 
 
 if __name__ == '__main__':
